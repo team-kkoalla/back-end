6daaf07549dba862a9dd26551d123ab5090e65bb
Tnalxmsk <alstnwkd990@naver.com>
2024-07-31 15:52:10 +0900

    feat: User Repository 구현
    유저 저장 기능 구현

---
+++ kkoalla-spring/src/main/java/com/kkoalla/kkoallaspring/repositroy/UserRepository.java
@@ -0,0 +1,21 @@
+package com.kkoalla.kkoallaspring.repositroy;
+
+import com.kkoalla.kkoallaspring.entity.User;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+
+    private final EntityManager em;
+
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    public User findById(Long id) {
+        return em.find(User.class, id);
+    }
+}
