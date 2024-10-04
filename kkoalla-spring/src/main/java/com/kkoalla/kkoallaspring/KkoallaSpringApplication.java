d83abcfb9aea5b7a750c8fdb84fa01481fed11aa
Tnalxmsk <alstnwkd990@naver.com>
2024-10-04 19:30:13 +0900

    feat: hello

--- kkoalla-spring/src/main/java/com/kkoalla/kkoallaspring/KkoallaSpringApplication.java
+++ kkoalla-spring/src/main/java/com/kkoalla/kkoallaspring/KkoallaSpringApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KkoallaSpringApplication {
-
+	// 안녕
 	public static void main(String[] args) {
 		SpringApplication.run(KkoallaSpringApplication.class, args);
 	}
